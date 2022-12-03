51b75e62f09b62c6e440a1e3b0f004157ed6a19f
waliurrahman <mwaliurrahman@gmail.com>
2022-12-04 03:06:23 +0600

    Removed 'Meta' Variable and set static

--- components/Meta.js
+++ components/Meta.js
@@ -5,8 +5,8 @@ const Meta = ({ title, keywords, description, icon }) => {
   return (
     <Head>
         <title>{title}</title>
-        <meta name='keywards' content={keywords} />
-        <meta name='description' content={description} />
+        <meta name='keywards' content='দৈনিক প্রথম কথা, বাংলা জাতীয় দৈনিক' />
+        <meta name='description' content='দৈনিক প্রথম কথা' />
         <link rel="icon" type="image/x-icon"  href={icon} />
     </Head>
   )
